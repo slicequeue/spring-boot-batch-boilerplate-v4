58d79043b4b9ba0abafc62e0914c9e447e10ea68
slicequeue <slicequeue@gmail.com>
2024-03-01 15:25:31 +0900

    fix: job 이름 변경 및 배치 파라미터 검증 추가

--- src/main/java/com/slicequeue/springboot/batch/job/HelloWorldJob.java
+++ src/main/java/com/slicequeue/springboot/batch/job/HelloWorldJob.java
@@ -3,16 +3,20 @@ package com.slicequeue.springboot.batch.job;
 import com.slicequeue.springboot.batch.job.validator.ParameterValidator;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.job.CompositeJobParametersValidator;
 import org.springframework.batch.core.job.DefaultJobParametersValidator;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 import java.util.Arrays;
 
+@EnableBatchProcessing
+@SpringBootApplication
 public class HelloWorldJob {
 
     @Autowired
@@ -26,8 +30,8 @@ public class HelloWorldJob {
         CompositeJobParametersValidator validator = new CompositeJobParametersValidator();
 
         DefaultJobParametersValidator defaultJobParametersValidator = new DefaultJobParametersValidator(
-                new String[]{"fileName"},
-                new String[]{"name", "currentDate"}
+                new String[]{"fileName"},               // required
+                new String[]{"name", "currentDate"}     // optional
         );
         defaultJobParametersValidator.afterPropertiesSet();
 
@@ -37,7 +41,8 @@ public class HelloWorldJob {
 
     @Bean
     public Job job() {
-        return this.jobBuilderFactory.get("basicJob")
+        return this.jobBuilderFactory.get("helloWorldJob")
+                .validator(validator())
                 .start(step1())
                 .build();
     }
